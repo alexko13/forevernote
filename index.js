7a74423c9b9314075f0ae5fbe571eae03028edb6
Alex Ko <alexko@utexas.edu>
2016-02-15 16:13:12 -0700

    added mongoose for mongo db

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost/forevernote');
 
 app.get('/', function(req, res) {
   res.end('Hello World!');
