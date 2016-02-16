554aa318034af8b2ea0ea2a5dd280c97b224201a
Alex Ko <alexko@utexas.edu>
2016-02-16 00:01:30 -0700

    can display persisted notes

--- client/note/note.js
+++ client/note/note.js
@@ -1,6 +1,7 @@
 angular.module('note', [])
 
 .controller('NoteController', function ($scope, Note) {
+  $scope.allNotes = [];
   $scope.note = {
     title: '',
     text: ''
@@ -10,6 +11,18 @@ angular.module('note', [])
     Note.saveNote($scope.note)
     .then(function(resp) {
       console.log(resp);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  };
+
+  $scope.getNotes = function() {
+    console.log('GET ALL NOTES ......');
+    Note.getNotes()
+    .then(function(resp) {
+      console.log(resp);
+      $scope.allNotes = resp.data;
     })
     .catch(function(err) {
       console.error(err);
