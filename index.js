a368095fa32715cd7c0520d7e05e8f095f300a2c
Alex Ko <alexko@utexas.edu>
2016-02-15 20:50:32 -0700

    updated to use seperate modules

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 var express = require('express');
-var db = require('./server/database');
+var database = require('./server/database');
 var middleware = require('./server/middleware');
 
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-
-
-
-db.connect();
+database.connect();
 middleware.use(app, express);
-
-
-app.route('/api/note')
-  .get(function(req, res, next) {
-    console.log('handling get in /api/note');
-    res.json('200');
-  })
-  .post(function (req, res, next) {
-    console.log(req.body);
-    res.json(req.body);
-  });
-
 
 
 app.listen(port, function() {
   console.log('Listening on port: ' + port);
 });
-
-
 
 module.exports = app;
