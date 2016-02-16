55d1b0a9fb7bf7ccacb42b8af00aa0b55e214a70
Alex Ko <alexko@utexas.edu>
2016-02-16 14:12:30 -0700

    updated saving notes to also update notebook references

--- server/note/noteController.js
+++ server/note/noteController.js
@@ -1,8 +1,20 @@
 var Note = require('./noteModel.js');
+var Notebook = require('../notebook/notebookModel.js');
 
 module.exports = {
   getNotes: function(req, res, next) {
-    Note.find({}, function(error, doc) {
+
+    // console.log('in back end notebook is', req.query._id);
+    // Notebook.findById(req.query._id)
+    // .populate('notes')
+    // .exec(function(error, doc) {
+    //   console.log('populated notebook', doc);
+    // });
+
+
+    Note.find({
+      //"notebook._id": req.query._id
+    }, function(error, doc) {
       if(error) {
         res.json(error);
       } else {
@@ -13,11 +25,25 @@ module.exports = {
   
   saveNote: function(req, res, next) {
     var note = req.body;
-    Note.create(note, function(error, doc) {
+    console.log(req.body);
+    Note.create(note, function(error, savedNote) {
       if(error) {
         res.json(error);
       } else {
-        res.json(doc);
+        Notebook.findById(note.notebook._id, function(error, savedNotebook) {
+          if(error) {
+            res.json(error);
+          } else {
+            savedNotebook.notes.push(savedNote);
+            savedNotebook.save(function(error) {
+              if(error)  {
+                res.json(error);
+              } else {
+                res.json(savedNote);
+              }
+            });
+          }
+        });
       }
     });
   }
