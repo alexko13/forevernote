94490b2be3d8556da5476a1585c69791c0ad6bd1
Alex Ko <alexko@utexas.edu>
2016-02-15 21:04:37 -0700

    added basic anular routing

--- client/app.js
+++ client/app.js
@@ -1,5 +1,14 @@
 angular.module('forevernote', [
   'note',
   'ngRoute'
-]);
-
+  ])
+.config(function($routeProvider) {
+  $routeProvider
+  .when('/new-note', {
+    templateUrl: 'note/new-note.html',
+    controller: 'NoteController'
+  })
+  .otherwise({
+    redirectTo: '/new-note'
+  });
+});
